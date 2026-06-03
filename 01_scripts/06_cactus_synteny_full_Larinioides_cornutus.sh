b6962f45c85ee0ff3b8e1a2ce14d4abf3da693fb
lmnano <54070997+lmnano@users.noreply.github.com>
2026-06-03 10:42:14 +0200

    Add files via upload

---
+++ 01_scripts/06_cactus_synteny_full_Larinioides_cornutus.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ALIGNMENT=../00_data/05.1_cactusFull/05.1_cactusFull.hal
+TARGET=Larinioides_cornutus
+
+QUERYLIST=06_synteny_list_full_$TARGET.txt
+QUERIES=$(cat $QUERYLIST)
+
+OUT=../00_data/06.1_cactus_synteny_full
+
+mkdir $OUT
+mkdir $OUT/$TARGET
+
+for q in $QUERIES;
+do
+	OUTFILE=06.1_synteny_$TARGET\_"$q".psl
+
+	singularity exec -e -B $(realpath ..) ../cactus_v3.1.3.sif halSynteny \
+		--queryGenome "$q" \
+		--targetGenome $TARGET \
+		$ALIGNMENT \
+		$OUT/$TARGET/$OUTFILE
+
+done
